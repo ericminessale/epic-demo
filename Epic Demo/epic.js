9c902b4d814c7e39c324294c667ceeccac036f6f
Eric Minessale <107222637+ericminessale@users.noreply.github.com>
2024-04-04 12:13:46 -0400

    Update epic.js

--- Epic Demo/epic.js
+++ Epic Demo/epic.js
@@ -1,13 +1,12 @@
-// Load libraries and configure variables
 var express = require('express');
 var fs = require('fs');
 var uuid = require('uuid');
 const qs = require('qs');
 var axios = require('axios');
 const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser'); // Import body-parser
+const bodyParser = require('body-parser');
 
-// Initialize Express app
+// Initialize Express
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -21,10 +20,9 @@ try {
   serviceKey = fs.readFileSync(privateKeyPath, 'utf8');
 } catch (err) {
   console.error('Error loading private key:', err);
-  process.exit(1); // Exit the process if key loading fails
+  process.exit(1);
 }
 
-// Use body-parser middleware to parse JSON data
 app.use(bodyParser.json());
 
 // Start the Express server
@@ -32,21 +30,25 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// Utility function to create expiration date
 function createExpirationDate() {
   var t = Date.now();
   t += 1000 * 60 * 5; // 5 minutes
   return Math.round(t / 1000);
 }
 
-// Define a POST route to trigger the FHIR request and generate JWT
+// POST route
 app.post('/', function(req, res) {
-  const { patient_first_name, patient_last_name, patient_birthdate } = req.body; // Now req.body should be defined
+  const { patient_first_name, patient_last_name, patient_birthdate, address, telecom } = req.body;
 
-  // Log the received data
-  console.log('Received data:', req.body);
+  // Received data debug log
+  console.log('Received data:', {
+    patient_first_name,
+    patient_last_name,
+    patient_birthdate,
+    address,
+    telecom
+  });
 
-  // Construct JWT claims
   var myUUID = uuid.v4();
   var payloadClaims = {
     "iss": serviceName,
@@ -56,10 +58,9 @@ app.post('/', function(req, res) {
     "exp": createExpirationDate()
   };
 
-  // Sign the JWT using jsonwebtoken
+  // Sign JWT
   var token = jwt.sign(payloadClaims, serviceKey, { algorithm: 'RS384' });
 
-  // Build Axios config for FHIR endpoint
   let data = qs.stringify({
     'grant_type': 'client_credentials',
     'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
@@ -78,12 +79,17 @@ app.post('/', function(req, res) {
     data: data
   };
 
-  // Trigger FHIR request using Axios
+  // FHIR request
   axios.request(config)
     .then((response) => {
       const accessToken = response.data.access_token;
 
-      const fhirUrl = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Patient?family=${patient_last_name}&given=${patient_first_name}&birthdate=${patient_birthdate}`;
+      let fhirUrl = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Patient?`;
+      if (patient_last_name) fhirUrl += `family=${patient_last_name}&`;
+      if (patient_first_name) fhirUrl += `given=${patient_first_name}&`;
+      if (patient_birthdate) fhirUrl += `birthdate=${patient_birthdate}&`;
+      if (address) fhirUrl += `address=${address}&`;
+      if (telecom) fhirUrl += `telecom=${telecom}&`;
 
       axios.get(fhirUrl, {
         headers: {
@@ -93,52 +99,55 @@ app.post('/', function(req, res) {
         }
       })
         .then((fhirResponse) => {
-          const patientResource = fhirResponse.data.entry[0].resource;
+          if (fhirResponse.data && fhirResponse.data.entry && fhirResponse.data.entry.length > 0) {
+            const patientResource = fhirResponse.data.entry[0].resource;
 
-          const patientData = {
-            id: patientResource.id,
-            name: `${patientResource.name[0].given[0]} ${patientResource.name[0].family}`,
-            gender: patientResource.gender,
-            birthDate: patientResource.birthDate,
-            address: patientResource.address[0].line.join(', ')
-          };
+            const patientData = {
+              id: patientResource.id,
+              name: `${patientResource.name[0].given[0]} ${patientResource.name[0].family}`,
+              gender: patientResource.gender,
+              birthDate: patientResource.birthDate,
+              address: patientResource.address[0].line.join(', ')
+            };
 
-          console.log('Patient Data:', patientData); // Log the patient data
+            console.log('Patient Data:', patientData);
 
-          // Now fetch appointments for the patient
-          const appointmentUrl = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Appointment?patient=${patientData.id}`;
+            // get appointments
+            const appointmentUrl = `https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/Appointment?patient=${patientData.id}`;
 
-          axios.get(appointmentUrl, {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-              'Content-Type': 'application/fhir+json',
-              Accept: 'application/json'
-            }
-          })
+            axios.get(appointmentUrl, {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+                'Content-Type': 'application/fhir+json',
+                Accept: 'application/json'
+              }
+            })
             .then((appointmentResponse) => {
               const appointments = appointmentResponse.data.entry.map(entry => entry.resource);
-              console.log('Appointments:', appointments); // Log the appointments
+              console.log('Appointments:', appointments);
               
-              // Extract specific appointment data
               const appointmentData = appointments.map(appointment => ({
                 id: appointment.id,
                 date: appointment.start,
                 status: appointment.status,
-                // Add more properties as needed
               }));
 
-              // Add appointmentData to patientData
+              // Add appointments to patient data
               patientData.appointments = appointmentData;
 
-              res.status(200).json(patientData); // Send the patient data with appointments back as the response
+              res.status(200).json(patientData); // Send the patient data and appointments
             })
             .catch((error) => {
               console.error('Error fetching appointments:', error);
               res.status(500).send('Error fetching appointments');
             });
+          } else {
+            console.log('Patient not found or no data returned.');
+            res.status(404).json({ error: 'Patient not found or no data returned.' });
+          }
         })
         .catch((error) => {
-          console.log('Error performing FHIR search:', error); // Log the error
+          console.log('Error performing FHIR search:', error); 
           res.status(500).send('Error performing FHIR search');
         });
     })
